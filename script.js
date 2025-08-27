e40a199773459f575534e0092ee2183a35f96e92
sivabalan-27 <siva270410@gmail.com>
2025-08-27 15:06:29 +0530

    update js

---
+++ script.js
@@ -0,0 +1,11 @@
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("close-nav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-180px"
+})
